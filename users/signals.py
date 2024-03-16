a2c7fd56b5bd2deab42c37fe8b44fb68f359e50f
DavidBugger <bwehfukdavid@gmail.com>
2024-03-16 08:54:36 +0100

    added some models

--- users/signals.py
+++ users/signals.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Profile
+from .models import Profile, Location
 
 # this creates user's profile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(users=instance)
+
+@receiver(post_save, sender=Profile)
+def create_profile_location(sender, instance, created, **kwargs):
+    if created:
+        profile_location = Location.objects.create()
+        instance.location = profile_location
+        instance.save()
+
+
